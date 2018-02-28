4c62f6c1b5fbe5f126c7344b8e78e24395ac4a98
fanhl <fanhonglin@klicen.com>
2018-02-28 19:33:08 +0800

    code

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # CustomBehaviorDemo
 
 自定义 Behavior
+
+参考 [CoordinatorLayout高级用法-自定义Behavior](http://blog.csdn.net/qibin0506/article/details/50290421)
--- app/src/main/java/com/fanhl/custombehaviordemo/dependent/DependentActivity.kt
+++ app/src/main/java/com/fanhl/custombehaviordemo/dependent/DependentActivity.kt
@@ -3,11 +3,8 @@ package com.fanhl.custombehaviordemo.dependent
 import android.os.Bundle
 import android.support.v4.view.ViewCompat
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
-import android.widget.TextView
 import com.fanhl.custombehaviordemo.R
+import kotlinx.android.synthetic.main.activity_dependent.*
 
 /**
  * 现在我们就来根据第一种情况尝试自定义一个Behavior，这里我们实现一个简单的效果，让一个View根据另一个View上下移动。
@@ -19,27 +16,8 @@ class DependentActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dependent)
 
-        val depentent = findViewById<View>(R.id.depentent) as TextView
-        depentent.setOnClickListener { v ->
+        dependent.setOnClickListener { v ->
             ViewCompat.offsetTopAndBottom(v, 5)
-        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-
-        return when (item.itemId) {
-            R.id.action_settings ->
-                return true
-            else -> super.onOptionsItemSelected(item)
         }
     }
 }
